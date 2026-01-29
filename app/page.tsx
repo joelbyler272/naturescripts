96ef3ab3542e2d7fd86f28c5600ea7fb2f656d2e
joelbyler272 <joelbyler272@gmail.com>
2026-01-28 23:31:13 -0500

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-// Root page redirects to marketing landing page
-// This file exists because Next.js requires a page.tsx at app/ level
-// The actual landing page is in app/(marketing)/page.tsx
-
-export { default } from './(marketing)/page'
