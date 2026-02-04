d317ee5a34985fb252df6d527f8eb8c78c436066
joelbyler272 <joelbyler272@gmail.com>
2026-02-04 18:01:51 -0500

    feat: Add OG image, apple touch icon, robots.txt, sitemap, skeleton exports

---
+++ components/skeletons/index.ts
@@ -0,0 +1,4 @@
+export { DashboardSkeleton } from './DashboardSkeleton';
+export { ProtocolsSkeleton } from './ProtocolsSkeleton';
+export { ProtocolDetailSkeleton } from './ProtocolDetailSkeleton';
+export { SettingsSkeleton } from './SettingsSkeleton';
